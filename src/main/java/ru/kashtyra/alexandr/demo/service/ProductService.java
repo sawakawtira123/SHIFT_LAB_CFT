57f90d85e873b5116828a6ef965f66051177eb37
Alexandr <48391762+sawakawtira123@users.noreply.github.com>
2021-02-27 23:03:25 +0700

    Update ProductService.java

--- src/main/java/ru/kashtyra/alexandr/demo/service/ProductService.java
+++ src/main/java/ru/kashtyra/alexandr/demo/service/ProductService.java
@@ -30,31 +30,7 @@ public class ProductService {
         this.productRepository = productRepository;
         this.typeProductRepository = typeProductRepository;
         this.propertiesRepository = propertiesRepository;
-        addTypeCategory(new TypeProduct("Настольные компьютеры"));
-        addTypeCategory(new TypeProduct("Ноутбук"));
-        addTypeCategory(new TypeProduct("Мониторы"));
-        addTypeCategory(new TypeProduct("Жесткие диски"));
-        addProperty(new Properties("13 Дюймовые"));
-        addProperty(new Properties("14 Дюймовые"));
-        addProperty(new Properties("15 Дюймовые"));
-        addProperty(new Properties("16 Дюймовые"));
-        addProperty(new Properties("17 Дюймовые"));
-        addProperty(new Properties("17 Диагональ"));
-        addProperty(new Properties("15 Диагональ"));
-        addProperty(new Properties("16 Диагональ"));
-        addProperty(new Properties("17 Диагональ"));
-        addProperty(new Properties("18 Диагональ"));
-        addProperty(new Properties("Объем 512"));
-        addProperty(new Properties("Объем 1024"));
-        addProperty(new Properties("Объем 2048"));
-        addProperty(new Properties("Объем 258"));
-        addProperty(new Properties("Десктопы"));
-        addProperty(new Properties("неттопы"));
-        addProperty(new Properties("моноблоки"));
-        add(new Product(1, 2425, 115000, 5, "HP", "Настольные компьютеры", "десктопы"));
-        add(new Product(2, 3426, 150000, 3, "ASUS", "Ноутбук", "16 Дюймовые"));
-        add(new Product(3, 4427, 25000, 3, "Apple", "Мониторы", "16 Диагональ"));
-        add(new Product(4, 5428, 5000, 12, "ACER", "Жесткие диски", "512 Объем"));
+       
     }
 
     private static Supplier<? extends DataAccessException> getSupplier(Integer productId) {
